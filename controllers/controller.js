154bf5c9f535d110843a12abe5807e86f7bf9b14
unknown <you@example.com>
2020-11-16 11:38:51 +0100

    makes more sense now

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,52 +5,74 @@ var showText;
 var checkMe = true;
 //you trigger everything
 document.getElementById('create').addEventListener('click', createCar);
-document.getElementById('cleanField').addEventListener('click', clearFields);
+document.getElementById('create').addEventListener('click', showDiv);
+document.getElementById('createWheels').addEventListener('click', createWheels);
 function createCar() {
     //data
     var plate = document.getElementById('plateCar').value;
     var brand = document.getElementById('inputBrand').value;
     var color = document.getElementById('colorCar').value;
-    //check my plate
-    var truePlate;
-    truePlate = plateValidate(plate);
-    //creation of object car
-    myCar = new Car(plate, color, brand);
-    console.log(myCar);
-    // data wheels
-    var diameter = document.getElementById('inputWheel').value;
-    var brandWheel = document.getElementById('brandWheels').value;
-    // new Object
-    myWheel = new Wheel(diameter, brandWheel);
-    //you'll tell me the true
-    checkMe;
-    //check later
-    //car.addWheel(new Wheel(diameter,brandWheel));
-    showText = "CAR: PLATE: " + myCar.plate
-        + " COLOR: " + myCar.color + " BRAND: " + myCar.brand
-        + " WHEELS: " + myWheel.toString();
-    // JSON.stringify(car.wheels)
-    console.log(myWheel.toString());
-    //remove display none
+    // element to show/hide
     var cardShow = document.getElementById('showCar');
-    (checkMe) ? cardShow.classList.remove('d-none') : cardShow.classList.add('d-none');
-    document.getElementById('answerMe').innerText = showText;
-}
-// clear fields
-function clearFields() {
-    console.log('I work clean');
-    (document.getElementById('plateCar')).innerHTML = "";
-    // documentacion
-    // https://www.iditect.com/how-to/53742045.html
-}
-//check my plate
-function plateValidate(plate) {
-    var plate_regex = /^([0-9]{4,4}[a-z]{3,3})$/i;
-    if (!plate_regex.test(plate)) {
-        alert("La matricula debe contener 4 numeros y 3 letras");
-        return false;
+    //check my plate
+    checkMe = plateValidate(plate);
+    //creation of object car
+    if (!checkMe) {
+        alert('Plate must have 4 numbers and 3 letters');
     }
     else {
-        return true;
+        myCar = new Car(plate, color, brand);
+        console.log(myCar);
+        console.log(myCar.plate);
+        console.log(myCar.color);
+        console.log(myCar.brand);
     }
+    showText = "CAR: PLATE: " + myCar.plate + "\n"
+        + " COLOR: " + myCar.color + "\n" + " BRAND: " + myCar.brand;
+    //remove display none
+    (checkMe) ? cardShow.classList.remove('d-none') : cardShow.classList.add('d-none');
+    document.getElementById('answerMeCar').innerText = showText;
 }
+//wheels
+function createWheels() {
+    console.log('wheels works');
+    // data wheels
+    var diameter = parseInt(document.getElementById('inputWheel').value);
+    var brandWheel = document.getElementById('brandWheels').value;
+    //checkWheel
+    checkMe = sizeWheel(diameter);
+    (!checkMe) ? alert("Wheel's size MUST be between 17 & 21") :
+        // new Object
+        myWheel = new Wheel(diameter, brandWheel);
+    console.log(myWheel.toString());
+    //grab me
+    var paragraph = document.getElementById('answerMeWheels');
+    //you can be shown
+    (checkMe) ? paragraph.classList.remove('d-none') : paragraph.classList.add('d-none');
+    //write it down
+    showText = myWheel.toString();
+    //print it
+    document.getElementById('answerMeWheels').textContent = showText;
+}
+//show the div
+function showDiv() {
+    (checkMe) ? document.getElementById('sizeWheelsCar').classList.remove('d-none') :
+        document.getElementById('sizeWheelsCar').classList.add('d-none');
+}
+//You'll tell me the whole true
+var myTrue;
+//check my plate
+function plateValidate(plate) {
+    var plateRegex = /^([0-9]{4,4}[a-z]{3,3})$/i;
+    (!plateRegex.test(plate)) ? myTrue = false : myTrue = true;
+    // tell me the true
+    return myTrue;
+}
+//check my size
+function sizeWheel(diameter) {
+    (diameter < 17 || diameter > 21) ? myTrue = false : myTrue = true;
+    //tell me the true 
+    return myTrue;
+}
+// documentacion
+// https://www.iditect.com/how-to/53742045.html
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -6,7 +6,8 @@ var checkMe: boolean = true;
 
  //you trigger everything
   document.getElementById('create').addEventListener('click', createCar);
-  document.getElementById('cleanField').addEventListener('click', clearFields);
+  document.getElementById('create').addEventListener('click', showDiv);
+  document.getElementById('createWheels').addEventListener('click', createWheels);
 
 function createCar(){
     
@@ -14,50 +15,71 @@ function createCar(){
  let plate: string = (document.getElementById('plateCar') as HTMLInputElement).value;
  let brand: string = (document.getElementById('inputBrand') as HTMLInputElement).value;
  let color: string = (document.getElementById('colorCar') as HTMLInputElement).value;
+    // element to show/hide
+let cardShow = document.getElementById('showCar');
 
 //check my plate
-var truePlate:boolean;
-truePlate = plateValidate(plate);
+checkMe = plateValidate(plate);
     //creation of object car
+if (!checkMe) {
+    alert('Plate must have 4 numbers and 3 letters');
+
+} else {
     myCar = new Car(plate,color,brand);
     console.log(myCar);
+    console.log(myCar.plate);
+    console.log(myCar.color);
+    console.log(myCar.brand);
+}
 
+    showText = "CAR: PLATE: " + myCar.plate + "\n"
+    + " COLOR: " +myCar.color + "\n" + " BRAND: " + myCar.brand ;
+        //remove display none
+   (checkMe) ? cardShow.classList.remove('d-none') : cardShow.classList.add('d-none'); 
+   document.getElementById('answerMeCar').innerText = showText;
+
+}
+//wheels
+function createWheels(){
+console.log('wheels works');
 // data wheels
-let diameter: any      = document.getElementById('inputWheel').value;
-let brandWheel: string = document.getElementById('brandWheels').value;
+let diameter: number      = parseInt((document.getElementById('inputWheel') as HTMLInputElement).value);
+let brandWheel: string = (document.getElementById('brandWheels') as HTMLInputElement).value;
+//checkWheel
+checkMe = sizeWheel(diameter);
+
+(!checkMe) ? alert("Wheel's size MUST be between 17 & 21") :
 // new Object
 myWheel = new Wheel(diameter, brandWheel);
-//you'll tell me the true
-checkMe;
-    //check later
-    //car.addWheel(new Wheel(diameter,brandWheel));
-
-    showText = "CAR: PLATE: " + myCar.plate 
-    + " COLOR: " +myCar.color + " BRAND: " + myCar.brand 
-    + " WHEELS: " + myWheel.toString();
-    // JSON.stringify(car.wheels)
-    console.log(myWheel.toString());
-    //remove display none
-    let cardShow = document.getElementById('showCar');
-   (checkMe) ? cardShow.classList.remove('d-none') : cardShow.classList.add('d-none'); 
-    document.getElementById('answerMe').innerText = showText;
+console.log(myWheel.toString());
+//grab me
+let paragraph: any = document.getElementById('answerMeWheels') as HTMLInputElement;
+//you can be shown
+(checkMe) ? paragraph.classList.remove('d-none') : paragraph.classList.add('d-none');
+//write it down
+showText = myWheel.toString();
+//print it
+document.getElementById('answerMeWheels').textContent = showText;
+}  
+//show the div
+function showDiv(){
+   (checkMe) ? document.getElementById('sizeWheelsCar').classList.remove('d-none') :
+               document.getElementById('sizeWheelsCar').classList.add('d-none');
 }
-
-// clear fields
-function clearFields(): void {
-    console.log('I work clean');
-    (document.getElementById('plateCar')).innerHTML = "";
-    
-    // documentacion
-    // https://www.iditect.com/how-to/53742045.html
-}
+//You'll tell me the whole true
+var myTrue: boolean;
 //check my plate
 function plateValidate(plate:string){
-    var plate_regex =  /^([0-9]{4,4}[a-z]{3,3})$/i; 
-    if (!plate_regex.test(plate)) {
-        alert("La matricula debe contener 4 numeros y 3 letras");
-        return false;
-    }else{
-        return true;
-    }
+    var plateRegex =  /^([0-9]{4,4}[a-z]{3,3})$/i; 
+    (!plateRegex.test(plate)) ? myTrue = false : myTrue = true;
+    // tell me the true
+    return myTrue;
 }
+//check my size
+function sizeWheel(diameter:number) {
+    (diameter < 17 || diameter > 21) ? myTrue = false : myTrue = true;
+    //tell me the true 
+    return myTrue;
+}
+    // documentacion
+    // https://www.iditect.com/how-to/53742045.html
